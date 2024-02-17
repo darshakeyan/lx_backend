a7b98dbbe48d0219ff1789dd93ecb4ea8245d270
darshakeyan <darshakparmar01.workspace@gmail.com>
2024-02-17 21:35:52 +0530

    env change

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-
-mongoose.connect(process.env.MONGO_URL).then(
+mongoose.connect(process.env.ATLAS_URI).then(
     () => {
         console.log("Connected to Database");
     }
 ).catch((error) => {
     console.log("Error Connecting to Database", error); 
-})
+});
